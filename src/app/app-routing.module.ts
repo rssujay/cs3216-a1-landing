89dc748cfce68eb4e63e1ced56824831a6c74255
nwjbrandon <nwjbrandon@gmail.com>
2019-08-30 12:58:03 +0800

    second block done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { FaqComponent } from './faq/faq.component';
-import { FeaturesComponent } from './features/features.component';
 import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
-  { path: 'faq', component: FaqComponent },
-  { path: 'features', component: FeaturesComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-import { FeaturesComponent } from './features/features.component';
-import { FaqComponent } from './faq/faq.component';
-import { ContactComponent } from './contact/contact.component';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -22,9 +19,6 @@ registerLocaleData(en);
   declarations: [
     AppComponent,
     AboutComponent,
-    FeaturesComponent,
-    FaqComponent,
-    ContactComponent,
     HomeComponent
   ],
   imports: [
